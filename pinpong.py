68ef03f166fb35f00a002c44245a33e2fc2d5e00
blackYmove <135245686+blackYmove@users.noreply.github.com>
2023-06-08 17:13:22 +0300

    pinpong.py

--- pinpong.py
+++ pinpong.py
@@ -4,7 +4,8 @@ wndw = display.set_mode((600, 500))
 wndw.fill((255, 255, 255))
 clock = time.Clock()
 finish = False
-
+speed_x = 3
+speed_y = 3
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed, size):
         super().__init__()
@@ -20,26 +21,47 @@ class Player(GameSprite):
         keys = key.get_pressed()
         if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_s] and self.rect.y < 550:
+        if keys[K_s] and self.rect.y < 340:
             self.rect.y += self.speed
-class Player(GameSprite):
     def updater(self):
         keys = key.get_pressed()
         if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.y < 550:
+        if keys[K_DOWN] and self.rect.y < 340:
             self.rect.y += self.speed
 
-pl1 = Player('ракоетка.png', 100, 100, 40, (150, 25))
-pl2 = Player('ракоетка.png', 100, 100, 40, (150, 25))
-
-
-
+pl1 = Player('ракоетка.png', 550, 100, 4, (25, 150))
+pl2 = Player('ракоетка.png', 5, 100, 4, (25, 150))
+myach = GameSprite('мяч.png', 250, 250, 5, (50,50))
+font.init()
+fontt = font.Font(None, 35)
+win1 = fontt.render('игрок 1 победил', True, (180, 0, 0))
+win2 = fontt.render('игрок 2 победил', True, (180, 0, 0)) 
 
 while game != False:
+    
     for e in event.get():
         if e.type == QUIT:
-            game = False  
+            game = False 
+    if finish != True:
+        wndw.fill((255, 255, 255))
+        myach.rect.x += speed_x
+        myach.rect.y += speed_y
+        pl1.updater()
+        pl1.reset()
+        pl2.updatel()
+        pl2.reset()
+        myach.reset()
+    if myach.rect.y > 450 or myach.rect.y < 0:
+        speed_y *= -1
+    if sprite.collide_rect(pl1, myach) or sprite.collide_rect(pl2, myach):
+        speed_x *= -1
+    if myach.rect.x < 0:
+        finish = True
+        wndw.blit(win1, (200, 200))
+    if myach.rect.x > 550:
+        finish = True
+        wndw.blit(win2, (200, 200))
 
 
     
